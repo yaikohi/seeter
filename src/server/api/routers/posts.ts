4caf3619bc3a8ecd39b8e1d1de979e18a6566ac7
yaikohi <erik.beem@hotmail.com>
2023-04-29 12:29:11 +0200

    change to clerk
    - add shadcn styling
    - remove nextauth stuff

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany();
+  }),
+});
