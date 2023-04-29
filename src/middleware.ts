e49e74064adfdd0e2888133b83eb615fba3046b6
yaikohi <erik.beem@hotmail.com>
2023-04-29 13:02:20 +0200

    fix: trpc / clerk integration
    - WIP: post mutation

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,15 @@ import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export default withClerkMiddleware(() => {
-    console.log('middleware running?')
+  console.log("middleware running?");
   return NextResponse.next();
 });
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher: [
+    "/((?!_next/image|_next/static|favicon.ico).*)",
+    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
+    // "/",
+  ],
 };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,24 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    });
+
+    console.log(users);
+
+    return posts;
   }),
 });
