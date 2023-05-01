2fb07b6225356aaeb09e8d656d02d7694ded46d7
yaikohi <erik.beem@hotmail.com>
2023-05-01 15:45:29 +0200

    commented out unused code!!! for now!

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,60 +2,60 @@ import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "../trpc";
 import { clerkClient } from "@clerk/nextjs/server";
-import { type Post } from "@prisma/client";
-import { type User } from "@clerk/nextjs/dist/api";
-import { TRPCError } from "@trpc/server";
-import { RouterOutputs } from "~/utils/api";
+// import { type Post } from "@prisma/client";
+// import { type User } from "@clerk/nextjs/dist/api";
+// import { TRPCError } from "@trpc/server";
+// import { RouterOutputs } from "~/utils/api";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-    externalUsername:
-      user.externalAccounts.find(
-        (externalAccount) => externalAccount.provider === "oauth_github"
-      )?.username || null,
-  };
-};
+// const filterUserForClient = (user: User) => {
+//   return {
+//     id: user.id,
+//     username: user.username,
+//     profileImageUrl: user.profileImageUrl,
+//     externalUsername:
+//       user.externalAccounts.find(
+//         (externalAccount) => externalAccount.provider === "oauth_github"
+//       )?.username || null,
+//   };
+// };
 
-const addUserDataToPosts = async (posts: Post[]) => {
-  const userId = posts.map((post) => post.authorId);
-  const users = (
-    await clerkClient.users.getUserList({
-      userId: userId,
-      limit: 110,
-    })
-  ).map(filterUserForClient);
+// const addUserDataToPosts = async (posts: Post[]) => {
+//   const userId = posts.map((post) => post.authorId);
+//   const users = (
+//     await clerkClient.users.getUserList({
+//       userId: userId,
+//       limit: 110,
+//     })
+//   ).map(filterUserForClient);
 
-  return posts.map((post) => {
-    const author = users.find((user) => user.id === post.authorId);
-    if (!author) {
-      console.error("AUTHOR NOT FOUND");
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
-      });
-    }
-    if (!author.username) {
-      // user the ExternalUsername
-      if (!author.externalUsername) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: `Author has no GitHub Account: ${author.id}`,
-        });
-      }
-      author.username = author.externalUsername;
-    }
-    return {
-      post,
-      author: {
-        ...author,
-        username: author.username ?? "(username not found)",
-      },
-    };
-  });
-};
+//   return posts.map((post) => {
+//     const author = users.find((user) => user.id === post.authorId);
+//     if (!author) {
+//       console.error("AUTHOR NOT FOUND");
+//       throw new TRPCError({
+//         code: "INTERNAL_SERVER_ERROR",
+//         message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
+//       });
+//     }
+//     if (!author.username) {
+//       // user the ExternalUsername
+//       if (!author.externalUsername) {
+//         throw new TRPCError({
+//           code: "INTERNAL_SERVER_ERROR",
+//           message: `Author has no GitHub Account: ${author.id}`,
+//         });
+//       }
+//       author.username = author.externalUsername;
+//     }
+//     return {
+//       post,
+//       author: {
+//         ...author,
+//         username: author.username ?? "(username not found)",
+//       },
+//     };
+//   });
+// };
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
