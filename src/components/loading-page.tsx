f54a144b279af45e5685bde89a349ea84daec7e7
yaikohi <erik.beem@hotmail.com>
2023-05-02 11:38:10 +0200

    fix styling loading page

--- src/components/loading-page.tsx
+++ src/components/loading-page.tsx
@@ -2,7 +2,7 @@ import { LoadingSpinner } from "./ui/loading-spinner";
 
 export const LoadingPage = () => {
   return (
-    <div className="absolute inset-0 h-full w-full bg-gradient-to-b from-white to-slate-600">
+    <div className="absolute inset-0 h-full w-full  bg-opacity-40 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-muted to-background">
       <div className="absolute left-1/2 top-1/3">
         <LoadingSpinner />
       </div>
