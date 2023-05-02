e7b1b37464f162998ae572ba2ae55dd4a8235643
yaikohi <erik.beem@hotmail.com>
2023-05-02 11:38:53 +0200

    fix loading spinner colours

--- src/components/ui/loading-spinner.tsx
+++ src/components/ui/loading-spinner.tsx
@@ -3,7 +3,7 @@ export const LoadingSpinner = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+        className="mr-2 h-8 w-8 animate-spin fill-secondary text-secondary-foreground"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
