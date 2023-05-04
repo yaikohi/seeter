3b45da7621250a4d933fa975076f74db33755191
yaikohi <erik.beem@hotmail.com>
2023-05-04 09:59:44 +0200

    add dropdown + dialogue to delete option of seethe

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -3,6 +3,49 @@ import type { RouterOutputs } from "~/utils/api";
 import { intlFormatDistance } from "date-fns";
 import { Fade } from "react-awesome-reveal";
 import Link from "next/link";
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  MoreHorizontal,
+  Plus,
+  PlusCircle,
+  Settings,
+  Trash,
+  Trash2,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 export type PostGetAllOutput = RouterOutputs["posts"]["getAll"];
 
 export function Feed({ posts }: { posts: PostGetAllOutput }) {
@@ -35,9 +78,9 @@ export function Post(props: PostProps) {
   return (
     <div className="flex flex-col rounded-xl bg-muted p-2 opacity-100 transition-opacity duration-200 delay-200">
       <PostName post={props.post} />
-      <Link href={`/s/${id}`}>
-        <p className="ml-4 break-words">{content}</p>
-      </Link>
+      {/* <Link href={`/s/${id}`}> */}
+      <p className="ml-4 break-words">{content}</p>
+      {/* </Link > */}
     </div>
   );
 }
@@ -47,15 +90,46 @@ export function PostName(props: PostProps) {
     post: { username, createdAt },
   } = props;
   return (
-    <div className="flex place-items-center gap-2 px-2 py-1 text-sm ">
-      <Link
-        href={`/@${username as string}`}
-        className="text-base font-bold tracking-tight hover:underline"
-      >{`@${username as string}`}</Link>
-      <span className="font-light">·</span>
-      <span className="font-light">
-        {intlFormatDistance(createdAt, new Date())}
-      </span>
+    <div className="flex place-items-center justify-between gap-2 px-2 py-1 text-sm">
+      <div className="flex place-items-center gap-2">
+        <Link
+          href={`/@${username as string}`}
+          className="text-base font-bold tracking-tight hover:underline"
+        >{`@${username as string}`}</Link>
+        <span className="font-light">·</span>
+        <span className="font-light">
+          {intlFormatDistance(createdAt, new Date())}
+        </span>
+      </div>
+      <Dialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MoreHorizontal />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DialogTrigger asChild>
+              <DropdownMenuItem>
+                <DropdownMenuItem className="flex place-items-center gap-2">
+                  <Trash2 />
+                  <span> Delete</span>
+                </DropdownMenuItem>
+              </DropdownMenuItem>
+            </DialogTrigger>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. Are you sure you want to permanently
+              delete this seethe from our servers?
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button type="submit">Delete seethe</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
