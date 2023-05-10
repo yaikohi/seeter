41d826569ac0984b2cafe175cef41a40afd231c8
yaikohi <erik.beem@hotmail.com>
2023-05-10 10:58:40 +0200

    style: fixed dark/light theming

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,35 +1,8 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
-import { dark, neobrutalism } from "@clerk/themes";
-import { useThemeContext } from "./context/theme";
 export function UserNav() {
-  const { theme, toggleTheme } = useThemeContext();
   return (
     <SignedIn>
-      <UserButton
-        appearance={{
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          baseTheme: theme === "dark" ? dark : neobrutalism,
-          // variables: {
-          //   borderRadius: "var(--radius)",
-          //   colorAlphaShade: "black",
-          //   colorBackground: "var(--background)",
-          //   colorDanger: "var(--destructive)",
-          //   colorInputBackground: "",
-          //   colorInputText: "",
-          //   colorPrimary: "var(--primary)",
-          //   colorShimmer: "",
-          //   colorSuccess: "",
-          //   colorText: "var(--foreground)",
-          //   colorTextOnPrimaryBackground: "",
-          //   colorTextSecondary: "var(--secondary)",
-          //   colorWarning: "",
-          //   fontFamily: "inherit",
-          //   fontFamilyButtons: "inherit",
-          //   fontSize: "1.25rem",
-          //   fontSmoothing: "antialiased",
-          // }
-        }}
-      />
+      <UserButton />
     </SignedIn>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,28 @@ import "~/styles/globals.css";
 import { api } from "~/utils/api";
 import { Toaster } from "~/components/ui/toaster";
 import { ThemeContextProvider } from "~/components/context/theme";
+import { useRouter } from "next/router";
+import { dark } from "@clerk/themes";
+import React from "react";
 
+type Themes = "light" | "dark";
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const { push } = useRouter();
+
+  const theme =
+    typeof window !== "undefined"
+      ? (localStorage.getItem("theme") as Themes)
+      : "light";
+
   return (
     <ThemeContextProvider>
-      <ClerkProvider {...pageProps}>
+      <ClerkProvider
+        {...pageProps}
+        navigate={(to) => push(to)}
+        appearance={{
+          baseTheme: theme === "dark" ? dark : undefined,
+        }}
+      >
         <Component {...pageProps} />
         <Toaster />
       </ClerkProvider>
