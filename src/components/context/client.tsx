5f37a331f4fbb5e92faeb7a6fed6c838e5397b79
yaikohi <erik.beem@hotmail.com>
2023-05-10 14:34:28 +0200

    chore: add description to hook

--- src/components/context/client.tsx
+++ src/components/context/client.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+/**
+ * The effect runs when the component that uses this hook gets added to the page.
+ * That means that the moment this runs the component runs on the client.
+ */
 function useIsClient() {
   const [isClient, setClient] = React.useState(false);
 
