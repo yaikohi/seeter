20d7296bab26dc6e69fcb92f2632ee2e1e1a3207
yaikohi <erik.beem@hotmail.com>
2023-05-10 10:59:36 +0200

    Revert "style: fixed dark/light theming"
    This reverts commit 41d826569ac0984b2cafe175cef41a40afd231c8.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,28 +5,11 @@ import "~/styles/globals.css";
 import { api } from "~/utils/api";
 import { Toaster } from "~/components/ui/toaster";
 import { ThemeContextProvider } from "~/components/context/theme";
-import { useRouter } from "next/router";
-import { dark } from "@clerk/themes";
-import React from "react";
 
-type Themes = "light" | "dark";
 const MyApp: AppType = ({ Component, pageProps }) => {
-  const { push } = useRouter();
-
-  const theme =
-    typeof window !== "undefined"
-      ? (localStorage.getItem("theme") as Themes)
-      : "light";
-
   return (
     <ThemeContextProvider>
-      <ClerkProvider
-        {...pageProps}
-        navigate={(to) => push(to)}
-        appearance={{
-          baseTheme: theme === "dark" ? dark : undefined,
-        }}
-      >
+      <ClerkProvider {...pageProps}>
         <Component {...pageProps} />
         <Toaster />
       </ClerkProvider>
