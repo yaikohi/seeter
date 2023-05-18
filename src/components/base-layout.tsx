57081dcc15a441f7840087cadfdfdc0666ea23c7
yaikohi <erik.beem@hotmail.com>
2023-05-18 13:56:17 +0200

    style: make it better

--- src/components/base-layout.tsx
+++ src/components/base-layout.tsx
@@ -15,8 +15,12 @@ export const BaseLayout = (props: PropsWithChildren<object>) => {
         <div className="absolute inset-0 -z-30 bg-gradient-to-tl from-muted via-blue-200 to-violet-100 blur-3xl dark:via-blue-900 dark:to-violet-900"></div>
         <div className="z-100 m-8 mx-auto rounded-xl bg-background/40 px-4 py-2 shadow-2xl shadow-transparent backdrop-blur-3xl md:max-w-4xl">
           <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-          <div className="my-12 flex flex-col gap-8   ">
+          <div className="my-12 flex flex-col gap-8">
             <SignedOut>
+              <div className="">
+                <h2>Hello stranger!</h2>
+                <p>Please login with github to seethe!</p>
+              </div>
               <UserSignedOutHeader />
             </SignedOut>
             <SignedIn>
@@ -32,11 +36,11 @@ export const BaseLayout = (props: PropsWithChildren<object>) => {
 
 export function UserSignedOutHeader() {
   return (
-    <div className="flex justify-between">
-      <div className="">
-        <h2>Hello stranger!</h2>
-        <p>Please login with github to seethe!</p>
-      </div>
+    <div className="flex place-items-center justify-between">
+      <Flame className="invisible" />
+      <Link href="/">
+        <Flame className="place-self-center" />
+      </Link>
 
       <SignInButton>
         <Button className="" variant={"default"}>
@@ -50,7 +54,7 @@ export function UserSignedOutHeader() {
 export function UserSignedInHeader() {
   return (
     <div className="flex justify-between">
-      <Flame className="invisible place-self-center" />
+      <Flame className="invisible" />
       <Link href="/">
         <Flame className="place-self-center" />
       </Link>
