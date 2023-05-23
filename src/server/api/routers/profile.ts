140ad13d804a21aa2b314bcb07f1869461c53615
yaikohi <erik.beem@hotmail.com>
2023-05-23 15:03:46 +0200

    feat: implement follow/unfollow feature in backend

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -40,20 +40,28 @@ export const profileRouter = createTRPCRouter({
   getProfileById: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
-      return await ctx.prisma.profile.findUnique({
+      const userProfile = await ctx.prisma.profile.findUnique({
         where: {
           authorId: input.id,
         },
+        include: {
+          followedBy: true,
+          following: true,
+        },
       });
+
+      if (!userProfile) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Profile not found.",
+        });
+      }
+
+      return userProfile;
     }),
   updateProfile: privateProcedure
     .input(
       z.object({
-        username: z
-          .string()
-          // .min(4, "Username needs to be at least 4 characters long.")
-          .max(20, "Username can only be 20 characters long.")
-          .optional(),
         description: z
           .string()
           .max(100, "Description can only be 100 characters long.")
@@ -61,66 +69,44 @@ export const profileRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const { description, username } = input;
-
-      if (typeof description === "string" && !username) {
-        await ctx.prisma.profile.upsert({
-          where: { authorId: ctx.userId },
-          create: {
-            authorId: ctx.userId,
-            description: description || "",
-            username: username || "",
-          },
-          update: {
-            description: description,
-          },
-        });
-      }
-
-      if (typeof username === "string" && !description) {
-        await ctx.prisma.profile.upsert({
-          where: { authorId: ctx.userId },
-          create: {
-            authorId: ctx.userId,
-            description: description || "",
-            username: username || "",
-          },
-          update: {
-            username: username,
-          },
-        });
-      } else {
-        await ctx.prisma.profile.upsert({
-          where: { authorId: ctx.userId },
-          create: {
-            authorId: ctx.userId,
-            description: description || "",
-            username: username || "",
-          },
-          update: {
-            description: description,
-            username: username,
-          },
-        });
-      }
+      await ctx.prisma.profile.upsert({
+        where: { authorId: ctx.userId },
+        create: {
+          authorId: ctx.userId,
+          description: input.description || "",
+        },
+        update: {
+          description: input.description,
+        },
+      });
     }),
   followProfile: privateProcedure
     .input(z.object({ userToFollowId: z.string() }))
     .mutation(async ({ ctx, input }) => {
-      const { userToFollowId } = input;
-      console.dir({ userId: ctx.userId, userToFollowId });
       await ctx.prisma.profile.update({
         where: {
           authorId: ctx.userId,
         },
         data: {
           following: {
-            create: {
-              following: {
-                connect: {
-                  authorId: userToFollowId,
-                },
-              },
+            connect: {
+              authorId: input.userToFollowId,
+            },
+          },
+        },
+      });
+    }),
+  unFollowProfile: privateProcedure
+    .input(z.object({ userToUnfollowId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.profile.update({
+        where: {
+          authorId: ctx.userId,
+        },
+        data: {
+          following: {
+            disconnect: {
+              authorId: input.userToUnfollowId,
             },
           },
         },
