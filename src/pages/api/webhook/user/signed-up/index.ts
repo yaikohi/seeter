9e48031e2348e4359f3be8ee800a356f093a5221
yaikohi <erik.beem@hotmail.com>
2023-05-24 17:04:42 +0200

    WIP: add api routes for hooks

---
+++ src/pages/api/webhook/user/signed-up/index.ts
@@ -0,0 +1,55 @@
+import { TRPCError } from "@trpc/server";
+import { getHTTPStatusCodeFromError } from "@trpc/server/http";
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+// const exampleHeaders = {
+//   "svix-id": "msg_p5jXN8AQM9LWM0D4loKWxJek",
+//   "svix-timestamp": "1614265330",
+//   "svix-signature": "v1,g0hM9SsE+OTPJTGt/tmIKtSyZlE3uFJELVlNIOLJ1OE=",
+// };
+
+const createProfileOnSignUpHandler = (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (req.method === "POST") {
+    // Create context and caller
+
+    const sId = req.headers["svix-id"];
+    const sTimestamp = req.headers["svix-timestamp"];
+    const sSignature = req.headers["svix-signature"];
+
+    // const caller = appRouter.createCaller(ctx);
+
+    try {
+      if (sId && sTimestamp && sSignature) {
+        res.status(200).json({ sId, sTimestamp, sSignature });
+      }
+      res.status(200).json({ headers: req.headers });
+    } catch (cause) {
+      // Another error occured
+      console.error(cause);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  if (req.method === "GET") {
+    try {
+      const authorId = "123";
+      res.status(200).json({
+        message: `Profile for authorId: ${authorId} was successfully created!`,
+      });
+    } catch (cause) {
+      if (cause instanceof TRPCError) {
+        // An error from tRPC occured
+        const httpCode = getHTTPStatusCodeFromError(cause);
+        return res.status(httpCode).json(cause);
+      }
+      // Another error occured
+      console.error(cause);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+};
+
+export default createProfileOnSignUpHandler;
